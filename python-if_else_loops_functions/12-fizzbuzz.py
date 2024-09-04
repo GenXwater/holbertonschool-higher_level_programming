2c6f4715407b828d06a8e6c8bb6f6d8c90670735
GenXwater <louis.genty@outlook.com>
2024-09-04 16:03:34 +0200

    remove f

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,9 +2,9 @@
 def fizzbuzz():
     for n in range(1, 101):
         if n % 3 == 0:
-            print(f"Fizz ", end="")
+            print("Fizz ", end="")
         elif n % 5 == 0:
-            print(f"Buzz ", end="")
+            print("Buzz ", end="")
         elif n % 3 == 0 and n % 5 == 0:
             print("FizzBuzz ", end="")
         else:
