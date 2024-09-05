15909acd05a036f510179d8f579bd80fe4ff6be1
GenXwater <louis.genty@outlook.com>
2024-09-05 15:57:21 +0200

    test

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -2,12 +2,28 @@
 
 if __name__ == "__main__":
 
-    from calculator_1 import add, sub, mul, div
+    from calculator_1 import add, sub
 
     a = 10
     b = 5
 
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
+
+if __name__ == "__main__":
+
+    from calculator_1 import mul
+
+    a = 10
+    b = 5
+
     print("{} * {} = {}".format(a, b, mul(a, b)))
+
+if __name__ == "__main__":
+
+    from calculator_1 import div
+
+    a = 10
+    b = 5
+
     print("{} / {} = {}".format(a, b, div(a, b)))
