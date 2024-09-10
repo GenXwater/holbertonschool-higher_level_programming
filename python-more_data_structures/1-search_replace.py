df96a064b4375f335783a92dfb3fa063f480468f
GenXwater <louis.genty@outlook.com>
2024-09-10 15:26:43 +0200

    reverse function

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 # variante solution :
-# new_list = [search if item == replace else item for item in my_list]
+# new_list = [replace if item == search else item for item in my_list]
 #    return new_list
 
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
     for n in range(len(new_list)):
-        if new_list[n] == replace:
-            new_list[n] = search
+        if new_list[n] == search:
+            new_list[n] = replace
     return new_list
