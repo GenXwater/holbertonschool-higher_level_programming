8df8cc628215a4ff1a12aed22e7707ebd1ddd897
GenXwater <louis.genty@outlook.com>
2024-09-11 16:36:12 +0200

    test

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def print_sorted_dictionary(a_dictionary):
-    for n in sorted(a_dictionary):
-        print("{} : {}".format(n, a_dictionary[n]))
+    for orderkey in sorted(a_dictionary):
+        print("{}: {}".format(orderkey, a_dictionary[orderkey]))
