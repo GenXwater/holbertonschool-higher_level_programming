ad631e8db752ba4bce34723106e1f445116d13ac
GenXwater <louis.genty@outlook.com>
2024-09-11 13:51:19 +0200

    fix bug holberton

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -6,4 +6,4 @@
 #        new_list[n] = new_list[n] * number
 #    return (new_list)
 def multiply_list_map(my_list=[], number=0):
-    return (list(map(lambda x: x * number, my_list))
+    return list(map(lambda x: x * number, my_list))
