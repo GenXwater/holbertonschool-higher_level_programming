75a86deeae6bf10ec25204fe7f7e4749bfd3dcd4
GenXwater <louis.genty@outlook.com>
2024-09-11 13:51:00 +0200

    ok

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+
+# variante solution :
+#    new_list = my_list[:]
+#    for n in range(len(new_list)):
+#        new_list[n] = new_list[n] * number
+#    return (new_list)
 def multiply_list_map(my_list=[], number=0):
-    return (list(map((lambda a: a * number), my_list)))
+    return (list(map(lambda x: x * number, my_list))
