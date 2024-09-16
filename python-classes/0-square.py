8dca711988c224d696f2e0cbc07ba3dbc07e8e7a
GenXwater <louis.genty@outlook.com>
2024-09-16 10:57:43 +0200

    added doc

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-
+"""
+This module defines an empty class Square.
+"""
 class Square:
+     """
+    This class defines a square.
+    Currently, it is an empty class with no attributes or methods.
+    """
     pass
