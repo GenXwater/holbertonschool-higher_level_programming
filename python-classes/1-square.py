057b7a2e99b3901da41bc971ba0e92ce5a297cda
GenXwater <louis.genty@outlook.com>
2024-09-16 11:07:09 +0200

    Private instance attribute: size

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines an size class Square.
+"""
+
+
+class Square:
+    """
+    This class defines a size square.
+    """
+    def __init__(self, size):
+        self.__size = size  # Private attribute
