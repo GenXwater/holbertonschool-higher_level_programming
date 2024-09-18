b7dec2bcc154b77acd1ccfbe5bd6a0803426df9d
GenXwater <louis.genty@outlook.com>
2024-09-18 10:28:43 +0200

    added documentation

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -5,4 +5,9 @@ This module defines a class Rectangle with size and position management.
 
 
 class Rectangle:
+    """
+    This class defines a Rectangle with a private instance
+    attribute: size and position.
+    """
+
     pass
