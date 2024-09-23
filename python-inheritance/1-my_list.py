7c2154e5a27412ba419d89eb49b02b2c513dad1f
GenXwater <louis.genty@outlook.com>
2024-09-23 15:08:23 +0200

    test

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,10 +6,6 @@ list object
 
 class MyList(list):
     """subclass of list"""
-    def __init__(self):
-        """Initialize objet"""
-        super().__init__()
-
     def print_sorted(self):
         """print sorted list"""
         print(sorted(self))
