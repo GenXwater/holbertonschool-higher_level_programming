ae9d925cf114a6d84b35654926ce83d55df952a7
GenXwater <louis.genty@outlook.com>
2024-09-24 11:52:34 +0200

    added doctrings

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -5,11 +5,22 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """
-    Square class, inherits from Rectangle.
+    This class defines a square.
+
+    A square is a special case of a rectangle where all sides are equal.
+    It inherits from the Rectangle class.
     """
+
     def __init__(self, size):
+        """
+        Initialize a new Square instance.
+
+        Args:
+            size (int): The size of the square's sides.
+                        Must be a positive integer.
+
+        The size is validated and set as both the width and height
+        of the square using the parent Rectangle class.
+        """
         super().__init__(size, size)
         self.__size = size
-
-    def area(self):
-            return self.__size ** 2
