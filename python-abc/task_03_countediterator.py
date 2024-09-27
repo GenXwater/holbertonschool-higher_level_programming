e7c3092d413c615a7afe7c57c3977d88bbde3c66
GenXwater <louis.genty@outlook.com>
2024-09-27 11:06:48 +0200

    count iterator

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+"""
+CountedIterator - Keeping Track of Iteration
+"""
+
+
+class CountedIterator:
+    """
+    This class allows you to count the number of itérables until there are no more items.
+    """
+
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.count = 0
+
+    def __next__(self):
+        item = next(self.iterator)
+        self.count += 1
+        return item
+
+    def get_count(self):
+        return self.count
