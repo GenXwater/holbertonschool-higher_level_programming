81b51aa5e2c822000d4f8e2704f79135d72fbb24
GenXwater <louis.genty@outlook.com>
2024-09-30 15:19:36 +0200

    LOADS : a json string to be converted to a python object

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module for load the JSON file
+"""
+
+
+import json
+
+def from_json_string(my_str):
+    """
+    Args:
+    my_str: a json string to be converted to a python object.
+
+    Returns:
+    a python data structure
+    """
+    return json.loads(my_str)
