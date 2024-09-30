f0601abe1340f9c26cc2ca1b6a2552d677111ac9
GenXwater <louis.genty@outlook.com>
2024-09-30 22:42:31 +0200

    fix pycodestyle

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -3,10 +3,12 @@
 This module defines a Student class with JSON serialization capabilities.
 """
 
+
 class Student:
     """
     Defines a student with first_name, last_name, and age attributes.
     """
+
     def __init__(self, first_name, last_name, age):
         """
         Initializes the Student instance with the provided attributes.
@@ -25,12 +27,12 @@ class Student:
             stud_dict = {}
             for item in attrs:
                 if type(item) == str:
-                    if item == 'first_name':
-                        stud_dict['first_name'] = self.first_name
-                    if item == 'last_name':
-                        stud_dict['last_name'] = self.last_name
-                    if item == 'age':
-                        stud_dict['age'] = self.age
+                    if item == "first_name":
+                        stud_dict["first_name"] = self.first_name
+                    if item == "last_name":
+                        stud_dict["last_name"] = self.last_name
+                    if item == "age":
+                        stud_dict["age"] = self.age
             return stud_dict
         else:
             return self.__dict__
