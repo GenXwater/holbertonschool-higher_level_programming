2841e0fdcad4d5431e75b1bb6c1f2589c28ec381
GenXwater <louis.genty@outlook.com>
2024-09-30 22:45:43 +0200

    fix pycodestyle

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -8,7 +8,6 @@ class Student:
     """
     Defines a student with first_name, last_name, and age attributes.
     """
-
     def __init__(self, first_name, last_name, age):
         """
         Initializes the Student instance with the provided attributes.
@@ -20,18 +19,18 @@ class Student:
     def to_json(self, attrs=None):
         """
         Returns a dictionary representation of the Student instance.
-        If attrs is a list of strings, only the specified attributes are included.
+        If attrs is a list of strings only the specified attributes ar included
         Otherwise, all attributes are included.
         """
-        if type(attrs) == list:
+        if isinstance(attrs) == list:
             stud_dict = {}
             for item in attrs:
-                if type(item) == str:
+                if isinstance(item) == str:
                     if item == "first_name":
                         stud_dict["first_name"] = self.first_name
-                    if item == "last_name":
+                    elif item == "last_name":
                         stud_dict["last_name"] = self.last_name
-                    if item == "age":
+                    elif item == "age":
                         stud_dict["age"] = self.age
             return stud_dict
         else:
