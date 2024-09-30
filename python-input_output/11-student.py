925955ecd52f43f03e2fd8d4f73546866b83b90e
GenXwater <louis.genty@outlook.com>
2024-09-30 22:54:56 +0200

    reload_from_json

---
+++ python-input_output/11-student.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+This module defines a Student class with JSON serialization capabilities.
+"""
+
+
+class Student:
+    """
+    Defines a student with first_name, last_name, and age attributes.
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes the Student instance with the provided attributes.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns a dictionary representation of the Student instance.
+        If attrs is a list of strings only the specified attributes ar included
+        Otherwise, all attributes are included.
+        """
+        if isinstance(attrs, list):
+            stud_dict = {}
+            for item in attrs:
+                if isinstance(item, str):
+                    if item == "first_name":
+                        stud_dict["first_name"] = self.first_name
+                    elif item == "last_name":
+                        stud_dict["last_name"] = self.last_name
+                    elif item == "age":
+                        stud_dict["age"] = self.age
+            return stud_dict
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+            """
+            Replaces all attributes of the Student instance based on the dictionary json
+            """
+            for key, value in json.items():
+                setattr(self, key, value)
