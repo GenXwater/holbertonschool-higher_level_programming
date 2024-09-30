04ce4566c91a4ff1a37f00bcf70d9fd837428e72
GenXwater <louis.genty@outlook.com>
2024-09-30 20:03:22 +0200

    Module for load the JSON file

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module for load the JSON file
+"""
+
+
+import json
+
+def save_to_json_file(my_obj, filename):
+    """
+    Args:
+    my_obj: The Python object to be saved as JSON.
+    filename (str): The name of the file to write to.
+    """
+    with open(filename, 'w') as f:
+        json.dump(my_obj, f)
