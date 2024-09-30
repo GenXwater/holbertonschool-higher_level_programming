523b21fb66211b956a39e4686222a9ca5025eaac
GenXwater <louis.genty@outlook.com>
2024-09-30 11:11:01 +0200

    added docstring

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""Module for read file"""
+
+
 def read_file(filename=""):
     """
-    read file
+    read file with text in utf8
     """
     with open(filename, encoding='utf-8') as file:
         print(file.read())
