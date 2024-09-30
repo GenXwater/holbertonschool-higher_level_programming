ade833df0dcf9529d18d5a8e28aaea5c20498256
GenXwater <louis.genty@outlook.com>
2024-09-30 20:15:54 +0200

    function that creates an Object from a “JSON file”

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Learn JSON
+"""
+
+
+import json
+
+def load_from_json_file(filename):
+    """
+    Args:
+    filename (str): The name of the JSON file to read from
+    Return:
+    object: A Python object created from the JSON file
+    """
+    with open(filename, 'r') as f:
+        return json.load(f)
