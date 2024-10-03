8ad5111edad289825f77b6b2e8b0c56e040fb6b6
GenXwater <louis.genty@outlook.com>
2024-10-03 14:27:37 +0200

    added a README

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -24,3 +24,13 @@ def pascal_triangle(n):
         triangle += [row]
 
     return triangle
+
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+
+print_triangle(pascal_triangle(6))
