fbaaeee672483cd701d9e0e751ccd5e840c330a2
GenXwater <louis.genty@outlook.com>
2024-10-09 19:37:30 +0200

    added error on print post

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -7,15 +7,19 @@ import requests
 
 def fetch_and_print_posts():
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
-    print(f"Status Code: {response.status_code}")
-    for title in response.json():
-        print(title["title"])
+
+    if response.status_code == 200:
+            print(f"Status Code: {response.status_code}")
+            for title in response.json():
+                 print(title["title"])
+    else:
+         print(f"Error: {response.status_code}")
 
 
 def fetch_and_save_posts():
-
+    # Récupérer les données JSON de l'API
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
-
+    # Convertir les données JSON en une liste de dictionnaires
     posts = response.json()
 
     with open("posts.csv", mode="w", encoding="utf-8") as f:
