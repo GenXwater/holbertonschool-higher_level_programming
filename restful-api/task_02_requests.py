bfaef8d030f321f7ee49eb7a283b9f45b0c1ade1
GenXwater <louis.genty@outlook.com>
2024-10-09 19:27:32 +0200

    fix pycodestyle

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -6,19 +6,21 @@ import requests
 
 
 def fetch_and_print_posts():
-    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
     print(f"Status Code: {response.status_code}")
     for title in response.json():
-        print(title['title'])
+        print(title["title"])
+
 
 def fetch_and_save_posts():
-    response = requests.get('https://jsonplaceholder.typicode.com/posts')
-    
+
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+
     posts = response.json()
 
-    with open('posts.csv', mode='w', encoding='utf-8') as f:
+    with open("posts.csv", mode="w", encoding="utf-8") as f:
         # Définition des noms de colonne
-        name_colonne = ['id', 'title', 'body']
+        name_colonne = ["id", "title", "body"]
 
         # création d'un objet DictWriter avec le nom des colonnes
         writer = csv.DictWriter(f, fieldnames=name_colonne)
@@ -28,8 +30,9 @@ def fetch_and_save_posts():
 
         # Parcourir la liste des posts et écrire chaque post dans le csv
         for post in posts:
-            writer.writerow({
-                'id': post['id'],
-                'title': post['title'],
-                'body': post['body']
-            })
+            writer.writerow(
+                {
+                    "id": post["id"],
+                    "title": post["title"],
+                    "body": post["body"]}
+            )
