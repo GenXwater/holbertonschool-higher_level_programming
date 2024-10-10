e2b30ea84ce5cca10ec426ab1c933e512fe542b4
GenXwater <louis.genty@outlook.com>
2024-10-10 13:15:21 +0200

    added doctring

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -20,22 +20,22 @@ def fetch_and_print_posts():
 
 def fetch_and_save_posts():
     """ Fetch and save"""
-    # Récupérer les données JSON de l'API
+    """Récupérer les données JSON de l'API"""
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
-    # Convertir les données JSON en une liste de dictionnaires
+    """Convertir les données JSON en une liste de dictionnaires"""
     posts = response.json()
 
     with open("posts.csv", mode="w", encoding="utf-8") as f:
-        # Définition des noms de colonne
+        """Définition des noms de colonne"""
         name_colonne = ["id", "title", "body"]
 
-        # création d'un objet DictWriter avec le nom des colonnes
+        """création d'un objet DictWriter avec le nom des colonnes"""
         writer = csv.DictWriter(f, fieldnames=name_colonne)
 
-        # Écrire les en-têtes dans le fichier csv
+        """Écrire les en-têtes dans le fichier csv"""
         writer.writeheader
 
-        # Parcourir la liste des posts et écrire chaque post dans le csv
+        """Parcourir la liste des posts et écrire chaque post dans le csv"""
         for post in posts:
             writer.writerow(
                 {
