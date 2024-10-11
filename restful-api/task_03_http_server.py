caf421400eb566fea77a3226aa3ad07b69143f1f
GenXwater <louis.genty@outlook.com>
2024-10-11 14:05:28 +0200

    test

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -30,12 +30,28 @@ class Server(Handler):
             self.wfile.write(b"Hello, this is a simple API!")
 
         elif self.path == "/data":
-            data = {"name": "John", "age": 30, "city": "New York"}
+            data = {
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+                }
             self.send_response(200)
             self.send_header("Content-Type", "application/json")
             self.end_headers()
             self.wfile.write(json.dumps(data).encode('utf-8'))
 
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
 
 
 if __name__ == "__main__":
