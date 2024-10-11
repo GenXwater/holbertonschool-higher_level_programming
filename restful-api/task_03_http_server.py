48a4a201eda3cff6aba6801243d0be90a6d61ad2
GenXwater <louis.genty@outlook.com>
2024-10-11 13:39:26 +0200

    fix pycodestyle

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+
+
 import http.server
 import json
 
@@ -11,18 +14,14 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
     - '/status' : returns 'OK' to check the status of the API.
     For any other undefined endpoints, it returns a 404 Not Found error.
     """
-    
+
     def do_GET(self):
-        if self.path == '/data':
+        if self.path == "/data":
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
 
-            response = {
-                "name": "John",
-                "age": 30,
-                "city": "New York"
-            }
+            response = {"name": "John", "age": 30, "city": "New York"}
 
             self.wfile.write(json.dumps(response).encode())
 
@@ -31,20 +30,21 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
-        
+
         elif self.path == "/status":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"OK")
-        
+
         else:
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"404 Not Found: Endpoint not found")
 
-server_address = ('', 8000)
+
+server_address = ("", 8000)
 httpd = http.server.HTTPServer(server_address, RequestHandler)
 
 httpd.serve_forever()
