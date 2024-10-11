bc9a1544f668f585d1557d68268df3c0cbcdb52a
GenXwater <louis.genty@outlook.com>
2024-10-11 15:02:27 +0200

    fix second

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -33,6 +33,9 @@ def fetch_and_save_posts():
             """création d'un objet DictWriter avec le nom des colonnes"""
             writer = csv.DictWriter(f, fieldnames=name_colonne)
 
+            """Écrire les en-têtes dans le fichier csv"""
+            writer.writeheader()
+
             """Parcourir la liste des posts et écrire chaque post dans le csv"""
             for post in posts:
                 writer.writerow(
