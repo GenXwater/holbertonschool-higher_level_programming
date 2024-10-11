f6c8d10a3f5a31f493b62f6ba95f01b2678b084f
GenXwater <louis.genty@outlook.com>
2024-10-11 15:01:47 +0200

    fix first commit

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -21,24 +21,24 @@ def fetch_and_save_posts():
     """Récupérer les données JSON de l'API"""
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
     """Convertir les données JSON en une liste de dictionnaires"""
-    posts = response.json()
 
-    with open("posts.csv", mode="w", encoding="utf-8") as f:
-        """Définition des noms de colonne"""
-        name_colonne = ["id", "title", "body"]
+    if response.status_code == 200:
+        
+        posts = response.json()
+        
+        with open("posts.csv", mode="w", encoding="utf-8") as f:
+            """Définition des noms de colonne"""
+            name_colonne = ["id", "title", "body"]
 
-        """création d'un objet DictWriter avec le nom des colonnes"""
-        writer = csv.DictWriter(f, fieldnames=name_colonne)
+            """création d'un objet DictWriter avec le nom des colonnes"""
+            writer = csv.DictWriter(f, fieldnames=name_colonne)
 
-        """Écrire les en-têtes dans le fichier csv"""
-        writer.writeheader
-
-        """Parcourir la liste des posts et écrire chaque post dans le csv"""
-        for post in posts:
-            writer.writerow(
-                {
-                    "id": post["id"],
-                    "title": post["title"],
-                    "body": post["body"]}
-            )
+            """Parcourir la liste des posts et écrire chaque post dans le csv"""
+            for post in posts:
+                writer.writerow(
+                    {
+                        "id": post["id"],
+                        "title": post["title"],
+                        "body": post["body"]}
+                )
  
